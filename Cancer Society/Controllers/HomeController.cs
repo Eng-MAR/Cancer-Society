bc2e921566b5eb89885557af2c2b56932870c63b
Eng-MAR <Drrahman911@hotmail.com>
2022-10-18 20:57:48 -0400

    Second Commit: Controller method named "Intro" is added to the Home Controller.

--- Cancer Society/Controllers/HomeController.cs
+++ Cancer Society/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace Cancer_Society.Controllers
         {
             return View();
         }
+        public IActionResult Intro()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
