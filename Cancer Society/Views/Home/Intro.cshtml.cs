27e085e124b2686bd9eb9dd4a536aa27cdb32bd4
Eng-MAR <Drrahman911@hotmail.com>
2022-10-21 00:00:26 -0400

    Sixth Commit: Paragraph is added and title has been set for the page.

---
+++ Cancer Society/Views/Home/Intro.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Cancer_Society.Views.Home
+{
+    public class IntroModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
